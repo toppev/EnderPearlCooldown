476a9137662181beee2270b699b282a88603afce
Toppe <thetoppe5@gmail.com>
2018-11-21 19:24:36 +0200

    first commit

---
+++ src/com/gmail/thetoppe5/enderpearlcooldown/EnderPearlCooldown.java
@@ -0,0 +1,154 @@
+package com.gmail.thetoppe5.enderpearlcooldown;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.text.DecimalFormat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class EnderPearlCooldown extends JavaPlugin implements CommandExecutor{
+
+
+	public static final String WEBSITE_URL = "https://api.spigotmc.org/legacy/update.php?resource=43307/";
+
+	private String pearlName;
+	private double cooldown;
+	private int interval;
+	private DecimalFormat format;
+	private boolean asyncUpdate, xpBar, updateAvailable;
+
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
+		load();
+		
+		new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				checkUpdate();
+			}
+		}.runTaskAsynchronously(this);
+	}
+
+	@Override
+	public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args) {
+		long st = System.currentTimeMillis();
+		reloadConfig();
+		load();
+		long et = System.currentTimeMillis();
+		sender.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "EnderPearlCooldown was reloaded in " + (et-st) + "ms.");
+		sender.sendMessage(ChatColor.DARK_AQUA + "Checking for updates...");
+
+		new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				checkUpdate();
+				if(updateAvailable && sender != null) {
+					sender.sendMessage(ChatColor.GRAY + "There is a new update available for " + getDescription().getName());
+					sender.sendMessage(ChatColor.GRAY + "Link: https://www.spigotmc.org/resources/43307/updates");
+				}
+			}
+		}.runTaskAsynchronously(this);
+		return true;
+	}
+
+	private void load() {
+		format = new DecimalFormat(getConfig().getString("format"));
+		pearlName = getConfig().getString("pearl-name");
+		cooldown = getConfig().getDouble("cooldown");
+		interval = getConfig().getInt("interval");
+		xpBar = getConfig().getBoolean("xp-bar");
+	}
+
+
+	private void checkUpdate() {
+		try {
+			URLConnection conn = new URL(WEBSITE_URL).openConnection();
+			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			conn.setConnectTimeout(10000);
+			conn.setReadTimeout(10000);
+			conn.connect();
+			BufferedReader localBufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream(), Charset.forName("UTF-8")));
+			StringBuilder localStringBuilder = new StringBuilder();
+			String str1;
+			while ((str1 = localBufferedReader.readLine()) != null) {
+				localStringBuilder.append(str1);
+			}
+			String str2 = localStringBuilder.toString();
+			if(!this.getDescription().getVersion().equals(str2)) {
+				updateAvailable = true;
+			}
+		}catch(IOException e) {
+			Bukkit.getLogger().warning("Failed to check updates for " + getDescription().getName() + " " + getDescription().getVersion());
+		}
+	}
+
+	public DecimalFormat getFormat() {
+		return format;
+	}
+
+	public void setFormat(DecimalFormat format) {
+		this.format = format;
+	}
+
+	public double getCooldown() {
+		return cooldown;
+	}
+
+	public void setCooldown(double cooldown) {
+		this.cooldown = cooldown;
+	}
+
+	public String getPearlName() {
+		return pearlName;
+	}
+
+	public void setPearlName(String pearlName) {
+		this.pearlName = pearlName;
+	}
+
+	public int getInterval() {
+		return interval;
+	}
+
+	public void setInterval(int task) {
+		this.interval = task;
+	}
+
+	public void setAsyncUpdate(boolean asyncUpdate) {
+		this.asyncUpdate = asyncUpdate;
+	}
+
+	public boolean isAsyncUpdate() {
+		return asyncUpdate;
+	}
+
+	public boolean isXpBar() {
+		return xpBar;
+	}
+
+	public void setXpBar(boolean xpBar) {
+		this.xpBar = xpBar;
+	}
+
+	public boolean isUpdateAvailable() {
+		return updateAvailable;
+	}
+
+	public void setUpdateAvailable(boolean updateAvailable) {
+		this.updateAvailable = updateAvailable;
+	}
+}
---
+++ src/com/gmail/thetoppe5/enderpearlcooldown/PlayerListener.java
@@ -0,0 +1,212 @@
+package com.gmail.thetoppe5.enderpearlcooldown;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class PlayerListener implements Listener {
+
+
+	public static final HashMap<UUID, Long> cooldown = new HashMap<UUID, Long>();
+
+	private final EnderPearlCooldown plugin;
+
+	public PlayerListener(EnderPearlCooldown plugin) {
+		this.plugin = plugin;
+	}
+
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e) {
+		for(ItemStack it : e.getPlayer().getInventory().getContents()) {
+			if(it != null && it.getType() == Material.ENDER_PEARL) {
+				ItemMeta iMeta = it.getItemMeta();
+				iMeta.setDisplayName(null);
+				it.setItemMeta(iMeta);
+			}
+		}
+		if(plugin.isXpBar()) {
+			e.getPlayer().setLevel(0);
+			e.getPlayer().setExp(0);
+		}
+	}
+
+
+	@EventHandler
+	public void onLaunch(PlayerInteractEvent e){
+		Player p = e.getPlayer();
+		if(e.getItem() != null && e.getItem().getType() == Material.ENDER_PEARL) {
+			if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+				if(cooldown.containsKey(p.getUniqueId())) {
+					double l = cooldown.get(p.getUniqueId());
+					double i = plugin.getCooldown()*1000;
+					double c = System.currentTimeMillis();
+					if(l+i > c) {
+						e.setCancelled(true);
+						double x = (l+i-c)/1000;
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("cooldown-message")).replace("<time>", plugin.getFormat().format(x)));
+						return;
+					}
+				}
+				cooldown.put(p.getUniqueId(), System.currentTimeMillis());
+				new PearlTask(p);
+			}
+		}
+	}
+
+
+	@EventHandler(priority=EventPriority.HIGHEST, ignoreCancelled=true)
+	public void onPickup(PlayerPickupItemEvent e) {
+		updateItem(e.getItem().getItemStack(), e.getPlayer());
+	}
+
+
+	@EventHandler
+	public void onItemHeld(PlayerItemHeldEvent e) {
+		Player p = e.getPlayer();
+		ItemStack item = p.getInventory().getItem(e.getNewSlot());
+		if(item != null && item.getType() == Material.ENDER_PEARL) {
+			updateItem(item, p);
+		}
+	}
+
+
+	private void updateItem(ItemStack item, Player p) {
+		if(item.getType() == Material.ENDER_PEARL) {
+			if(cooldown.containsKey(p.getUniqueId())) {
+				double l = cooldown.get(p.getUniqueId());
+				double i = plugin.getCooldown()*1000;
+				double c = System.currentTimeMillis();
+				if(l+i > c) {
+					double x = (l+i-c)/1000;
+					item.setItemMeta(getPearlItemMeta(item, plugin.getFormat().format(x)));
+					return;
+				}
+			}
+			ItemMeta im = item.getItemMeta();
+			im.setDisplayName(null);
+			item.setItemMeta(im);
+		}
+	}
+
+
+	@EventHandler(priority=EventPriority.HIGHEST, ignoreCancelled=true)
+	public void onClick(InventoryClickEvent e) {
+		ItemStack item = e.getCurrentItem();
+		if(item != null && item.getType() == Material.ENDER_PEARL) {
+			ItemMeta iMeta = item.getItemMeta();
+			iMeta.setDisplayName(null);
+			item.setItemMeta(iMeta);
+		}
+	}
+
+
+	private ItemMeta getPearlItemMeta(ItemStack item, String l) {
+		String s = ChatColor.translateAlternateColorCodes('&', plugin.getPearlName());
+		ItemMeta iMeta = item.getItemMeta();
+		iMeta.setDisplayName(s.replace("<time>", l));
+		return iMeta;
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		if(plugin.isUpdateAvailable() && e.getPlayer().isOp()) {
+			e.getPlayer().sendMessage(ChatColor.GRAY + "There is a new update available for " + plugin.getDescription().getName());
+			e.getPlayer().sendMessage(ChatColor.GRAY + "Link: https://www.spigotmc.org/resources/43307/updates");
+		}
+	}
+
+
+
+	private class PearlTask extends BukkitRunnable{
+
+		
+		private Player p;
+		private int counter;
+
+		public PearlTask(Player p) {
+			this.p = p;
+			counter = plugin.getInterval();
+			if(plugin.isAsyncUpdate()) {
+				runTaskTimerAsynchronously(plugin, 0, 1);
+			}
+			else {
+				runTaskTimer(plugin, 0, 1);
+			}
+		}
+		
+
+		@Override
+		public void run() {
+			if(p != null) {
+				long left = left(p);
+				if(plugin.isXpBar()) {
+					p.setLevel((int) (left/1000));
+					p.setExp((float) (left/(plugin.getCooldown()*1000)));
+				}
+				counter--;
+				if(counter == 0) {
+					counter = plugin.getInterval();
+					if(p.getItemInHand().getType() == Material.ENDER_PEARL) {
+						if(left > 0) {
+							double x = left/1000;
+							String format = plugin.getFormat().format(x);
+							for(ItemStack it : p.getInventory().getContents()) {
+								if(it != null && it.getType() == Material.ENDER_PEARL) {
+									it.setItemMeta(getPearlItemMeta(it, format));
+								}
+							}
+						}
+						else {
+							cooldown.remove(p.getUniqueId());
+							for(ItemStack it : p.getInventory().getContents()) {
+								if(it != null && it.getType() == Material.ENDER_PEARL) {
+									ItemMeta iMeta = it.getItemMeta();
+									iMeta.setDisplayName(null);
+									it.setItemMeta(iMeta);
+								}
+							}
+							cancel();
+						}
+					}
+				}
+			}
+			else cancel();
+		}
+		
+		@Override
+		public synchronized void cancel() throws IllegalStateException {
+			super.cancel();
+			this.p = null;
+		}
+		
+
+		private long left(Player p) {
+			if(cooldown.containsKey(p.getUniqueId())) {
+				double i = plugin.getCooldown()*1000;
+				double l = cooldown.get(p.getUniqueId());
+				long c = System.currentTimeMillis();
+				if(l+i > c) {
+					return (long) (l+i-c);
+				}
+			}
+			return 0;
+		}
+	}
+}
